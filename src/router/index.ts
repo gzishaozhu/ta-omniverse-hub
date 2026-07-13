c3daa7bbf7a94609e24d92f4b9d6b8e49ab91d12
gzishaozhu <210424616@qq.com>
2026-07-13 22:46:09 +0800

    Add src/router/index.ts

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+﻿import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/views/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: () => import('@/views/GalleryPage.vue'),
+    },
+    {
+      path: '/shader-studio',
+      name: 'shader-studio',
+      component: () => import('@/views/ShaderStudio.vue'),
+    },
+  ],
+})
+export default router
